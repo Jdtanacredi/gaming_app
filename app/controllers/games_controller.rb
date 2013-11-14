f764a34ed4a452bdbc772eb01f6d93d8e15ecf34
John Macaluso <macaluso.john@gmail.com>
2013-11-13 21:14:34 -0500

    Working rock paper scissor

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,4 +17,20 @@ class GamesController < ApplicationController
    end
   end
 
+  def rock_paper_scissors_play
+   @user_throw = params[:throw]
+   @app_throw = ["rock","paper","scissor"].sample
+   if @user_throw == @app_throw
+      @status = "you tied"
+   elsif @user_throw == "rock" && @app_throw == "paper"
+      @status = "you lose"
+   elsif @user_throw == "paper" && @app_throw == "scissor"
+      @status = "you lose"
+   elsif @user_throw == "scissor" && @app_throw == "rock"
+      @status = "you lose"
+   else
+      @status = "You Win!"
+   end
+  end
+
 end
